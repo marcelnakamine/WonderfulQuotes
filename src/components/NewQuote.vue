13e3ae56306bbb29f1982046379f06c4245de3db
Marcel Nakamine <marcel.nakamine@gmail.com>
2017-10-23 14:03:50 -0200

    Allowing users to create quotes with a NewQuote component

---
+++ src/components/NewQuote.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+  <div class="row">
+    <form>
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3">
+        <label>Quote</label>
+        <textarea class="form-control" name="quote" v-model="quote" rows="3" cols="80"></textarea>
+      </div>
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3">
+        <button class="brn btn-primary" @click.prevent="createNew">Add Quote</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      quote: ''
+    };
+  },
+  methods: {
+    createNew() {
+      
+    }
+  }
+}
+</script>
+
+<style lang="css">
+
+</style>
